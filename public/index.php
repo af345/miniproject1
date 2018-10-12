647ad9acd17d52df932bf6ec6b4dd17967962f14
af345 <af345@njit.edu>
2018-10-11 21:45:14 -0400

    Feature: getting code to read csv file

--- public/index.php
+++ public/index.php
@@ -6,114 +6,22 @@
 
 
 
-main::start("example.csv");
+main::start();
 
-class main {
+class main  {
 
-    static public function start($filename)  {
+    static public function start() {
 
-        $records = csv::getRecords($filename);
-        $table = html::generateTable($records);
-
-
-    }
-}
-
-class html {
-
-    public static function generateTable($records) {
-
-        $count = 0;
-
-        foreach ($records as $record) {
-
-            if($count == 0) {
-
-                $array = $record->returnArray();
-                $fields = array_keys($array);
-                $values = array_values($array);
-                print_r($fields);
-                print_r($values);
-
-            } else {
-                $array = $record->returnArray();
-                $values = array_values($array);
-                print_r($values);
-            }
-            $count++;
-        }
-    }
-}
-
-class csv {
-
-
-    static public function getRecords($filename) {
-
-        $file = fopen($filename,"r");
-
-        $fieldNames = array();
-
-        $count = 0;
-
+        $file=fopen("example.csv", "r");
 
         while(! feof($file))
         {
-
-            $record = fgetcsv($file);
-            if($count == 0) {
-                $fieldNames = $record;
-            } else {
-                $records[] = recordFactory::create($fieldNames, $record);
-            }
-            $count++;
+            print_r(fgetcsv($file));
         }
 
         fclose($file);
-        return $records;
-
     }
-
 }
-
-class record {
-
-    public function_contrust(Array $fieldNames = null, $values = null)
-    {
-        $record = array_combine($fieldNames, $values);
-
-        foreach ($record as $property => $value) {
-            $this ->createProperty($property, $value);
-        }
-
-    }
-
-        public function returnArray() {
-            $array = (array) $this;
-
-            return $array;
-    }
-
-        public function createProperty($name = 'first', $value = 'Alexandra') {
-
-            $this->{$name} = $value;
-
-        }
-    }
-
-    class recordFactory {
-
-        public static function create(Array $fieldNames = null, Array $values = null) {
-
-
-            $record = new record($fieldNames, $values);
-
-            return $record;
-
-        }
-    }
-
-
 
 
 
